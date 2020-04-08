8c7a198f7945125945fdc58eed5640676c0b0dfd
Konstantin Kolodiy <bonewell@yandex.ru>
2020-04-08 16:55:37 +0300

    Create skeleton

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "spfservice run\n";
+  return 0;
+}
---
+++ sfp_test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using ::testing::Eq;
+
+TEST(SPF, Calculate) {
+  EXPECT_THAT(true, Eq(true));
+}
